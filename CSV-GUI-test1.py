aa9d1b8f938fc7efac574becee58ad8c89c4e364
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-12-20 14:45:22 +0100

    Update CSV-GUI-test1.py

--- CSV-GUI-test1.py
+++ CSV-GUI-test1.py
@@ -6,34 +6,42 @@ import os
 
 class MainWindow(tk.Frame):
     
-    log_folder_location = 'C:\\Users\\C2204001\\Documents\\Logy/'
+    log_folder_location = 'C:\\Users\\C2204001\\Documents\\Logy'
     log_base_name = "CSV_analysis TEST"
-    i = 1
-    existing_logs = [f for f in os.listdir(log_folder_location) if f.startswith("CSV_analysis TEST")]
-    if existing_logs:
-        existing_numbers = [int(log.split('.')[1]) for log in existing_logs]
-        i = max(existing_numbers) + 1
-    else:
-        i = 1
-
-    log_name = f"{log_base_name}{i}.log"
-
-    log_location = os.path.join(log_folder_location,log_name)
-
-    logging.basicConfig(filename=log_location,level=logging.DEBUG)
     
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
         self.parent.title('CSV_analysis')
+
+        # Move the log-related code inside the __init__ method
+        self.i = 1
+        self.existing_logs = [f for f in os.listdir(self.log_folder_location) if f.startswith("CSV_analysis TEST")]
+        if self.existing_logs:
+            existing_numbers = [self.extract_numeric_part(log) for log in self.existing_logs]
+            self.i = max(existing_numbers) + 1
+        else:
+            self.i = 1
+
+        self.log_name = f"{self.log_base_name}{self.i}.log"
+        self.log_location = os.path.join(self.log_folder_location, self.log_name)
+
+        logging.basicConfig(filename=self.log_location, level=logging.DEBUG)
+
         self.create_widgets()
+
+    def extract_numeric_part(self, filename):
+        try:
+            return int(filename.split('.')[1])
+        except (IndexError, ValueError):
+            return 0
 
     def create_widgets(self):
         self.label = tk.Label(self, text='CSV Analýza')
         self.label.pack()
 
-        self.output_text = tk.Text(self, height=10, width=40)
-        self.output_text.pack()
+        self.output_text = tk.Text(self, wrap=tk.WORD, height=10, width=40)
+        self.output_text.pack(fill=tk.BOTH, expand=True)
 
         button_go = tk.Button(self, text='Go', width=25, command=self.load_csv)
         button_go.pack()
@@ -50,18 +58,18 @@ class MainWindow(tk.Frame):
                     if os.path.exists(file_path):
                         analysis = pd.read_csv(file_path)
                         output_str = str(analysis.head())
-                        self.output_text.delete(1.0, tk.END)  # Clear previous content
+                        self.output_text.delete(1.0, tk.END)
                         self.output_text.insert(tk.END, output_str)
-                except (FileNotFoundError) as err:
-                    logging.CRITICAL(f'File {file_path} ')
+                except FileNotFoundError:
+                    logging.CRITICAL(f'File {file_path} not found')
             except (SyntaxError, UnicodeError) as err:
                 logging.CRITICAL('Syntax or Unicode error recorded')
                 logging.ERROR(f'{err} recorded')
-                self.output_text.delete(1.0, tk.END)  # Clear previous content
+                self.output_text.delete(1.0, tk.END)
                 self.output_text.insert(tk.END, f'Error: {err}')
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = MainWindow(root)
-    app.pack()
+    app.pack(fill=tk.BOTH, expand=True)
     root.mainloop()
